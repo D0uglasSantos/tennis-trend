16330e9369771ae1a85c6c118578bd7e5e42e937
D0uglasSantos <douglasas410@gmail.com>
2024-05-29 21:03:12 -0300

    fix: fixing error props children in products

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -2,12 +2,11 @@ import Title from "../Title";
 import ProductCard from "./PrincipalCard";
 import Card from "./cards";
 
-
 const Products = ({ fotos = [] }) => {
   return (
     <section className=" flex flex-col items-center bg-white">
       <ProductCard />
-      <Title children="Veja outros Produtos em Promoção!" />
+      <Title>Veja outros produtos em promoção!!!</Title>
       <section className="px-8 pt-8 flex flex-grow flex-wrap gap-8 justify-around items-center">
         {fotos.map((foto, index) => (
           <Card foto={foto} key={index} />
